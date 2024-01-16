064e9169ae6a29aca579efc68d5c79fdf3817e0b
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-01-16 21:26:59 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+user_country_input = str(input("Country: "))
+user_follower_count_input = int(input("Follower Count: "))
+user_view_count_input = int(input("Proposed View Count: "))
+user_post_input = str(input("Proposed post: "))
+
